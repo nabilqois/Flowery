8065ce42a0a091d025ec0bfe1fa21340e0c96219
Nabil Qois <nabilqois91@gmail.com>
2022-05-23 22:56:51 +0700

    remove shadow on Bottom Navigation

--- app/src/main/java/com/nabil/flowery/ui/MainActivity.kt
+++ app/src/main/java/com/nabil/flowery/ui/MainActivity.kt
@@ -12,5 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNav.background = null
     }
 }
