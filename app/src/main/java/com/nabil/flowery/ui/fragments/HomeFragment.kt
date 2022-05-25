3887260b5e2b7093ca21102041d111aaec663730
Nabil Qois <nabilqois91@gmail.com>
2022-05-25 22:34:16 +0700

    add Trivia and Flower of The Day CardView

--- app/src/main/java/com/nabil/flowery/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/nabil/flowery/ui/fragments/HomeFragment.kt
@@ -6,15 +6,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nabil.flowery.R
+import com.nabil.flowery.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setTitleTrivia()
+    }
+
+    private fun setTitleTrivia() {
+        binding.tvTriviaTitle.text = "Tahukah Kamu ?"
+    }
+
+    private fun setDescTrivia() {
+        binding.tvTriviaDesc.text = "Bunga mawar memiliki ..."
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
