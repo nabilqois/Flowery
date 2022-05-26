0aa526b51d5ce2444378ccb9510f0b469442d24d
Nabil Qois <nabilqois91@gmail.com>
2022-05-26 20:43:04 +0700

    set up switch camera button

--- app/src/main/java/com/nabil/flowery/ui/CameraActivity.kt
+++ app/src/main/java/com/nabil/flowery/ui/CameraActivity.kt
@@ -25,7 +25,12 @@ class CameraActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.captureImage.setOnClickListener { takePhoto() }
-        binding.switchCamera.setOnClickListener { startCamera() }
+        binding.switchCamera.setOnClickListener {
+            cameraSelector = if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA
+            else CameraSelector.DEFAULT_BACK_CAMERA
+
+            startCamera()
+        }
     }
 
     public override fun onResume() {
