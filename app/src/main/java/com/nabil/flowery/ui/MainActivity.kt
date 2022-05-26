a21fdfe86d9dbe7d787c31a7972eb0e7e412432a
Nabil Qois <nabilqois91@gmail.com>
2022-05-26 15:14:21 +0700

    set up permission request for camera in MainActivity.kt

--- app/src/main/java/com/nabil/flowery/ui/MainActivity.kt
+++ app/src/main/java/com/nabil/flowery/ui/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.nabil.flowery.ui
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.nabil.flowery.R
 import com.nabil.flowery.databinding.ActivityMainBinding
@@ -36,6 +41,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
         binding.bottomNav.background = null
 
         changeFragment(homeFragment)
@@ -56,6 +69,29 @@ class MainActivity : AppCompatActivity() {
         binding.fabCamera.setOnClickListener { startCameraX() }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
     private fun changeFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, fragment)
@@ -69,5 +105,10 @@ class MainActivity : AppCompatActivity() {
     private fun startCameraX() {
         val intent = Intent(this, CameraActivity::class.java)
         startActivity(intent)
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 }
