f047b1e60544937673a2b81629b4f4ce4b0273b4
Nabil Qois <nabilqois91@gmail.com>
2022-05-26 15:02:44 +0700

    add functions on CameraActivity.kt

--- app/src/main/java/com/nabil/flowery/ui/CameraActivity.kt
+++ app/src/main/java/com/nabil/flowery/ui/CameraActivity.kt
@@ -1,16 +1,48 @@
 package com.nabil.flowery.ui
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.nabil.flowery.databinding.ActivityCameraBinding
 
 class CameraActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityCameraBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.captureImage.setOnClickListener { takePhoto() }
+        binding.switchCamera.setOnClickListener { startCamera() }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+        startCamera()
+    }
+
+    private fun takePhoto() {
+
+    }
+
+    private fun startCamera() {
+
+    }
+
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
