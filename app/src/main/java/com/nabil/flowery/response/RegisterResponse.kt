f103e08c3bad0b917e18da67d07978e39ad0919f
cytraark <rayghanimadhizky@gmail.com>
2022-05-28 14:48:49 +0700

    Cleaning Imports

--- app/src/main/java/com/nabil/flowery/response/RegisterResponse.kt
+++ app/src/main/java/com/nabil/flowery/response/RegisterResponse.kt
@@ -1,6 +1,10 @@
 package com.nabil.flowery.response
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class RegisterResponse(
     val error:Boolean,
     val message:String
-)
+):Parcelable
--- app/src/main/java/com/nabil/flowery/ui/LoginActivity.kt
+++ app/src/main/java/com/nabil/flowery/ui/LoginActivity.kt
@@ -4,13 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.nabil.flowery.R
 import com.nabil.flowery.databinding.ActivityLoginBinding
 import com.nabil.flowery.model.AuthModel
 import com.nabil.flowery.pref.UserPref
