0d16d6b7318b64d7cdee5e5d9d9f68c6ad8624bb
cytraark <rayghanimadhizky@gmail.com>
2022-06-06 15:27:10 +0700

    Moved the Alarm Functionality to Secondary Screen (No list of time)

--- app/src/main/java/com/nabil/flowery/receiver/AlarmReceiver.kt
+++ app/src/main/java/com/nabil/flowery/receiver/AlarmReceiver.kt
@@ -5,7 +5,6 @@ import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import com.nabil.flowery.R
 
--- app/src/main/java/com/nabil/flowery/ui/fragments/NotificationFragment.kt
+++ app/src/main/java/com/nabil/flowery/ui/fragments/NotificationFragment.kt
@@ -1,100 +1,41 @@
 package com.nabil.flowery.ui.fragments
 
-
-
-
-import android.app.*
-import android.content.Context
-import android.content.Context.NOTIFICATION_SERVICE
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.DatePicker
-import android.widget.TimePicker
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.nabil.flowery.databinding.FragmentNotificationBinding
-import com.nabil.flowery.receiver.AlarmReceiver
-import com.nabil.flowery.receiver.channelID
-import com.nabil.flowery.receiver.notificationID
-import com.nabil.flowery.ui.MainActivity
-import java.util.*
-
 
 class NotificationFragment : Fragment() {
 
     private lateinit var binding: FragmentNotificationBinding
-    private lateinit var datePicker: DatePicker
-    private lateinit var timePicker: TimePicker
-    private lateinit var button: Button
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentNotificationBinding.inflate(inflater, container, false)
         return binding.root
-
-
     }
-    @RequiresApi(Build.VERSION_CODES.O)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        timePicker = binding.TimePicker
-        datePicker = binding.DatePicker
-        button = binding.setAlarmButton
 
-        createNotificationChannel()
-        button.setOnClickListener{
-            setNotification()
-        }
+
+       binding.addButton.setOnClickListener{
+           changeFragment(functionFragment)
+       }
 
     }
 
-    private fun getTime(): Long{
-        val minute = timePicker.minute
-        val hour = timePicker.hour
-        val day = datePicker.dayOfMonth
-        val month = datePicker.month
-        val year = datePicker.year
-        val calendar: Calendar = Calendar.getInstance()
-
-        calendar.set(minute,hour,day,month,year)
-        return calendar.timeInMillis
+    private val functionFragment by lazy {
+        NotificationFunctionFragment()
     }
 
-    private fun createNotificationChannel(){
-        val name = "desc"
-        val desc = "Desc"
-        val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val channel = NotificationChannel(channelID,name,importance)
-        channel.description = desc
-        val notificationManager = activity?.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
+    private fun changeFragment(fragment:Fragment){
+        val transaction = parentFragmentManager.beginTransaction()
+        transaction.replace(com.nabil.flowery.R.id.fragment_container, fragment)
+        transaction.commit()
     }
 
-    private fun setNotification(){
-
-
-        val intent = Intent(context, AlarmReceiver::class.java)
-
-        val pendingIntent = PendingIntent.getBroadcast(
-            context,
-            notificationID,intent,PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-        )
-
-        val alarmManager = activity?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val time = getTime()
-        alarmManager.setExactAndAllowWhileIdle(
-            AlarmManager.RTC_WAKEUP,
-            time,
-            pendingIntent
-        )
-
-    }
 
 
 
